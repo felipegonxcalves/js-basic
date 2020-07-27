fe19339a2b8e48408eaa0e605d81185eb6ed24f5
felipeconxceicao <felipe.conceicao@infocast.com.br>
2020-07-27 17:32:50 -0300

    first commit

---
+++ incrementando-decrementando-valores.js
@@ -0,0 +1,32 @@
+var myValue = 5;
+
+myValue = myValue - 1;
+
+// A expressão é executada e o valor é decrementado depois.
+myValue--;
+
+// O valor é decrementado e a expressão é executada depois.
+--myValue;
+
+myValue++;
+
+++myValue;
+
+// Cada linha de comando é chamada de Expressão
+
+/*
+	EX:
+	var myValue = 5;
+
+	var newValue = myValue++;
+
+	res: myValue = 6;
+	res: newValue = 5;
+
+	var myValue = 5;
+
+	var newValue = ++myValue;
+
+	res: myValue = 6;
+	res: newValue = 6;
+*/
---
+++ operadores-atribuicao.js
@@ -0,0 +1,24 @@
+var myValue = 5;
+
+myValue = myValue + 2;
+myValue += 2;
+
+myValue = myValue - 2;
+myValue -= 2;
+
+myValue = myValue * 2;
+myValue *= 2;
+
+myValue = myValue / 2;
+myValue /= 2;
+
+myValue = myValue % 2;
+myValue %= 2;
+
+myValue = myValue ** 2;
+myValue **= 2;
+
+var myText = 'abc';
+
+myText = myText + 'def';
+myText += 'def';
